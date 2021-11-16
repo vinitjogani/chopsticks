2ae02268ba7d27125f094460bd92a9e7ca49e7e9
Vinit Jogani <vnjogani@gmail.com>
2021-11-16 03:59:23 +0000

    Initialize Svelte app

---
+++ ui/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+</script>
+
+<main>
+  <div id="computer">
+    <button>1</button>
+    <button>2</button>
+  </div>
+  <div id="you">
+    <button>1</button>
+    <button>1</button>
+  </div>
+</main>
+
+<style>
+  button {
+    background-image: url("hands.webp");
+  }
+</style>
